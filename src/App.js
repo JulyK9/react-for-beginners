637f7de9e70b1d5a8a31704c05424db5eb5062fd
JKNINE <jkesonjk@gmail.com>
2022-04-07 15:36:15 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route path="/movie/:id">
           <Detail />
