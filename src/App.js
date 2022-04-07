3fc2ec610b1df298103afc175eb4b33b5489309d
JKNINE <jkesonjk@gmail.com>
2022-04-07 12:42:32 +0900

    react movie app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// 이제 App.js는 router를 render 하는 역할 (router는 URL을 보고있는 commponent임)
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
